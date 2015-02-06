fca9f83251392819756fefcd7f050500a52a6f52
Brian Quistorff <bquistorff@gmail.com>
2015-02-06 12:49:05 -0500

    force md5 text mode

--- resources/bin/update_md5.sh
+++ resources/bin/update_md5.sh
@@ -2,9 +2,9 @@
 # Writes to .fname.md5 the md5, though only if wasn't correct before.
 
 p_md5name="$(dirname $1)/.$(basename $1).md5"
-md5sum $1 | cmp -s $p_md5name -
+md5sum -t $1 | cmp -s $p_md5name -
 if test $? -ne 0; then 
-	md5sum $1 > $p_md5name; 
+	md5sum -t $1 > $p_md5name; 
 fi
 
 if [ "$OS" = "Windows_NT" ]; then
