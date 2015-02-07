a4940e8c003174475461043e89043d97e2b6d54d
Brian Quistorff <bquistorff@gmail.com>
2015-02-07 09:46:28 -0500

    Several fixes dealing with md5s

--- fig/fig.mk
+++ fig/fig.mk
@@ -4,45 +4,70 @@
 # The figs that are too big in vector format and need to be rasterized
 #pngs_of_big_figs : fig/png/GE_placebo_dropped_map.png fig/png/RI_effects_map.png
 
+# Stata file lists
 #not all platforms can use stata to make pdf from gph. So use intermediate eps
 gphs := $(wildcard fig/gph/*.gph)
 gphs_to_eps_base := $(patsubst fig/gph/%.gph,fig/eps/%.eps,$(gphs))
 gphs_to_eps_notitle := $(patsubst fig/gph/%.gph,fig/eps/notitle/%_notitle.eps,$(gphs))
+gphs_to_pdfs_base := $(patsubst fig/gph/%.gph,fig/pdf/%.pdf,$(gphs))
+gphs_to_pdfs_notitle := $(patsubst fig/gph/%.gph,fig/pdf/notitle/%_notitle.pdf,$(gphs))
 .INTERMEDIATE : $(gphs_to_eps_base) $(gphs_to_eps_notitle)
 
-fig/eps/%.eps fig/eps/notitle/%_notitle.eps : fig/gph/%.gph
-	statab.sh do code/cli_gph_eps.do $*; \
-	mv cli_gph_eps.log temp/lastrun;
-	
-fig/pdf/%.pdf : fig/eps/%.eps
-	epstopdf --outfile=fig/pdf/$*.pdf fig/eps/$*.eps && \
-	  normalize_pdf.sh fig/pdf/$*.pdf && \
-	  update_md5.sh fig/pdf/$*.pdf
+ifeq "$(GENDEP_MD5)" "1"
+$(gphs_to_eps_base) : fig/eps/%.eps : fig/gph/.%.gph.md5
+else
+$(gphs_to_eps_base) : fig/eps/%.eps : fig/gph/%.gph
+endif
+	statab.sh do code/cli_gph_eps.do $* eps title; \
+	  mv cli_gph_eps.log temp/lastrun;
 
-#fig/png/%.png fig/png/notitle/%_notitle.png : fig/gph/%.gph
-#	if [ "$$OS" = "Windows_NT" ]; then \
-#		statab.sh do code/cli_gph_eps.do $* png; \
-#		mv cli_gph_eps.log temp/lastrun; \
-#	else \
-#		echo "Only works on Windows"; \
-#	fi
+ifeq "$(GENDEP_MD5)" "1"
+$(gphs_to_eps_notitle) : fig/eps/notitle/%_notitle.eps : fig/gph/.%.gph.md5
+else
+$(gphs_to_eps_notitle) : fig/eps/notitle/%_notitle.eps : fig/gph/%.gph
+endif
+	statab.sh do code/cli_gph_eps.do $* eps notitle; \
+	  mv cli_gph_eps.log temp/lastrun;
+	
+$(gphs_to_pdfs_base) $(gphs_to_pdfs_notitle) : fig/pdf/%.pdf : fig/eps/%.eps
+	epstopdf --outfile=fig/pdf/$*.pdf fig/eps/$*.eps;
+	normalize_pdf.sh fig/pdf/$*.pdf
+
 fig/png/%.png : fig/pdf/%.pdf	
 	$(GSEXE) -dSAFER -dBATCH -dNOPAUSE -sDEVICE=pnggray -r600 -sOutputFile=fig/png/$*.png fig/pdf/$*.pdf
 
-fig/svg/%.svg : fig/eps/%.eps
-	inkscape -f fig/eps/$*.eps --export-plain-svg=fig/svg/$*.svg  
+fig/svg/%.svg : fig/pdf/%.pdf
+	inkscape --without-gui -f fig/pdf/$*.pdf --export-plain-svg=fig/svg/$*.svg  
 
-.PHONY : pdfs_of_gphs pdfs_of_epss remove_orphan_pdfs
-gphs_to_pdfs_base := $(patsubst fig/gph/%.gph,fig/pdf/%.pdf,$(gphs))
-gphs_to_pdfs_notitle := $(patsubst fig/gph/%.gph,fig/pdf/notitle/%_notitle.pdf,$(gphs))
+.PHONY : pdfs_of_gphs pdfs_of_epss all_pdfs remove_orphan_pdfs
 pdfs_of_gphs : $(gphs_to_pdfs_base) $(gphs_to_pdfs_notitle)
 
+## Now the R-generation ones
 epss_base := $(wildcard fig/eps/*.eps)
 epss_to_pdfs_base := $(patsubst fig/eps/%.eps,fig/pdf/%.pdf,$(epss_base))
 epss_notitle := $(wildcard fig/eps/notitle/*.eps)
 epss_to_pdfs_notitle := $(patsubst fig/eps/notitle/%.eps,fig/pdf/notitle/%.pdf,$(epss_notitle))
 pdfs_of_epss : $(epss_to_pdfs_base) $(epss_to_pdfs_notitle)
 
+all_pdfs : pdfs_of_gphs pdfs_of_epss
+
+ifeq "$(GENDEP_MD5)" "1"
+$(epss_to_pdfs_base) : fig/pdf/%.pdf : fig/eps/.%.eps.md5
+else
+$(epss_to_pdfs_base) : fig/pdf/%.pdf : fig/eps/%.eps
+endif
+	epstopdf --outfile=fig/pdf/$*.pdf fig/eps/$*.eps;
+	normalize_pdf.sh fig/pdf/$*.pdf
+
+ifeq "$(GENDEP_MD5)" "1"
+$(epss_to_pdfs_notitle) : fig/pdf/notitle/%_notitle.pdf : fig/eps/notitle/.%_notitle.eps.md5
+else
+$(epss_to_pdfs_notitle) : fig/pdf/notitle/%_notitle.pdf : fig/eps/notitle/%_notitle.eps
+endif
+	epstopdf --outfile=fig/pdf/notitle/$*_notitle.pdf fig/eps/notitle/$*_notitle.eps;
+	normalize_pdf.sh fig/pdf/notitle/$*_notitle.pdf
+
+## Removal of orphaned PDFs (could do the other formats if need be).
 gphs_to_pdfs_base_nopath := $(patsubst fig/gph/%.gph,%.pdf,$(gphs))
 gphs_to_pdfs_notitle_nopath := $(patsubst fig/gph/%.gph,%_notitle.pdf,$(gphs))
 epss_to_pdfs_base_nopath := $(patsubst fig/eps/%.eps,%.pdf,$(epss_base))
@@ -50,7 +75,7 @@ epss_to_pdfs_notitle_nopath := $(patsubst fig/eps/%.eps,%_notitle.pdf,$(epss_bas
 
 #mv out the accompanied ones, then remove non-versioned.
 #the mv command below is big, so cd first to make it smaller. Should be more robust.
-remove_orphan_pdfs : pdfs_of_gphs pdfs_of_epss
+remove_orphan_pdfs : all_pdfs
 	mkdir -p temp/pdf; \
 	  mkdir -p temp/pdf/notitle; \
 	  cd fig/pdf && mv $(gphs_to_pdfs_base_nopath) $(epss_to_pdfs_base_nopath) ../../temp/pdf/ && \
--- resources/bin/make_deps_md5.sh
+++ resources/bin/make_deps_md5.sh
@@ -11,7 +11,8 @@ while read line; do
 		#echo "$word"
 		# All output dirs but log (since they are never intermediate files) 
 		# for the prerequisites that are outputs, substitute with MD5 format
-		if [[ $word =~ (data/|fig/gph/|fig/eps/|snippets/|tab/).* && "$LINE_SECOND" = "1" ]] ; then
+		#keep launchers in sync with make_deps_md5
+		if [[ $word =~ (data|fig/gph|fig/eps|snippets|tab)/.* && "$LINE_SECOND" = "1" ]] ; then
 			p_md5name="$(dirname $word)/.$(basename $word).md5"
 			echo -n "$p_md5name " >> $2
 		else
