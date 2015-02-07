6c0d83b8ff6802f1e0b3a6687d66d451bb2a7d13
Brian Quistorff <bquistorff@gmail.com>
2015-02-07 09:46:52 -0500

    small cleanup

--- writeups/writeups.mk
+++ writeups/writeups.mk
@@ -3,19 +3,26 @@
 
 -include writeups/.*.dep
 
+writeups/%.tex : writeups/%.lyx
+	cd writeups && lyx -e pdflatex $*.lyx
+
 HAVE_LATEXMK := $(shell command -v latexmk 2>/dev/null)
 
 ifneq "$(HAVE_LATEXMK)" ""
 
-writeups/%.tex : writeups/%.lyx
-	cd writeups && lyx -e pdflatex $*.lyx
 #note: using auxdir in the first latexmk line does work (compilation fails) so do a cleanup (-c) after.
 writeups/%.pdf : writeups/%.tex
 	echo "$(HAVE_LATEXMK)"
 	#we ignore this error so that the filtering still happens.
 	-cd writeups; latexmk -pdf -f -pdflatex="yes '' | pdflatex -interaction=nonstopmode" -use-make -deps -deps-out=$*.d $*.tex
 	cd writeups; cat $*.d | grep -v "\(texmf\|MiKTeX\|ProgramData\|temp\)"  > $*.d2 && mv $*.d2 $*.d
-	cd writeups; to_parent_dep.sh writeups $*.d .$*.dep
+	cd writeups; to_parent_dep.sh writeups $*.d .$*.dep && rm $*.d
+	if [ "$$GENDEP_MD5" = "1" ]; then \
+	    make_deps_md5.sh writeups/.$*.dep writeups/$*.temp.dep && mv writeups/$*.temp.dep writeups/.$*.dep; \
+	  fi;
+	if [ "$$OS" = "Windows_NT" ]; then \
+	    ATTRIB +H writeups/.$*.dep; \
+	  fi;
 	cd writeups; latexmk -c
 	normalize_pdf.sh writeups/$*.pdf
 #Alternative (only if have LyX on all platforms)
@@ -27,8 +34,6 @@ writeups/%.pdf : writeups/%.tex
 
 else
 
-writeups/%.tex : writeups/%.lyx
-	cd writeups && lyx -e pdflatex $*.lyx
 writeups/%.pdf : writeups/%.tex
 	cd writeups && pdflatex -interaction=nonstopmode -aux-directory=../temp $*.tex
 	normalize_pdf.sh writeups/$*.pdf
