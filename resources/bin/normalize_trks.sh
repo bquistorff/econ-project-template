b655224e57a87424fc42346d6e360f96260e3463
Brian Quistorff <bquistorff@gmail.com>
2015-02-15 11:51:13 -0500

    +complete normalize_trk rename

--- resources/bin/normalize_trks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Normalizes Stata *.trk files (stata.trk & backup.trk) to have unix line endings
-# Use this after installing modules if your project works across Windows and Unix platforms .
-# Usage: normalize_trks.sh code/ado
-
-the_d=$1
-
-sed -e '/^f /s/\\/\//g' -i $the_d/stata.trk
-sed -e '/^f /s/\\/\//g' -i $the_d/backup.trk
