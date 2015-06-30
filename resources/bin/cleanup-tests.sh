5b77ab460c0fe74c257b01b315627b03811fdacc
Brian Quistorff <bquistorff@gmail.com>
2015-06-30 17:53:24 -0400

    switch from notitle to bare

--- resources/bin/cleanup-tests.sh
+++ resources/bin/cleanup-tests.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #Cleanup at least the test stuff
 rm -f fig/eps/*test*
-rm -f fig/eps/notitle/*test*
+rm -f fig/eps/bare/*test*
 rm -f fig/gph/*test*
 rm -f snippets/*test*
 rm -f tab/tex/*test*
 rm -f log/*test* 
-rm -f log/smcl/*test* 
-rm -f log/raw/*test* 
+rm -f log/do/*test* 
