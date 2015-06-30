e7ff16f01015d530f8bc038a0d1c8cb89731ff0f
Brian Quistorff <bquistorff@gmail.com>
2015-06-30 18:14:15 -0400

    switch to no-smcl

--- resources/bin/last_status.sh
+++ resources/bin/last_status.sh
@@ -5,7 +5,7 @@
 # To do: If handy, expand to other log types
  
 ffirst=`ls -1t log/smcl/ | head -1`
-ftime=`cat log/smcl/$ffirst | grep ' {txt}opened on:' | sed -e 's/.\+{res}\(.\+\)/\1/g' | sed -e 's/,//g'`
+ftime=`cat log/smcl/$ffirst | grep ' opened on:' | sed -e 's/.\+\(.\+\)/\1/g' | sed -e 's/,//g'`
 delta_s=$(( $(date +%s) - $(date -d "$ftime" +%s) ))
 delta_m=$(($delta_s/60+1))
 find . -mmin -$delta_m -not -path "*/.git/*" -type f -print
